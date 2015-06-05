43cf3d887a05da4c144519dbbd370855b3f1f253
Sergio Bruder <sergio.bruder@westwing.com.br>
2015-06-05 13:40:55 -0300

    updated to centos 6.6. and virtualbox 4.3.28

--- vars.sh
+++ vars.sh
@@ -1,7 +1,7 @@
-NAME=centos65-x86_64
+NAME=centos66-x86_64
 TYPE=RedHat_64
-INSTALLER="./isos/CentOS-6.5-x86_64-minimal.iso"
-GUESTADDITIONS="./isos/VBoxGuestAdditions_4.3.14.iso"
+INSTALLER="./isos/CentOS-6.6-x86_64-minimal.iso"
+GUESTADDITIONS="./isos/VBoxGuestAdditions_4.3.28.iso"
 HDD="${HOME}/VirtualBox VMs/${NAME}/main.vdi"
 HDD_SWAP="${HOME}/VirtualBox VMs/${NAME}/swap.vdi"
 NATNET=10.0.2.0/24
